971d00cd95edeea7115f8ac1430134f0c41eef4d
d33fur <d33fur@gmail.com>
2024-03-27 15:39:00 +0300

    added: scylladb

--- server/conanfile.py
+++ server/conanfile.py
@@ -8,3 +8,4 @@ class Audio2WaveformConan(ConanFile):
     # self.requires("boost/1.84.0")
     self.requires("libpqxx/7.9.0")
     self.requires("nlohmann_json/3.11.3")
+    self.requires("redis-plus-plus/1.3.12")
